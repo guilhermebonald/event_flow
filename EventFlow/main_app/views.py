b3f8e92acd0045eb76bbb6fdcb552ba6a0900f50
Guilherme Bonald <guilhermebonald@hotmail.com>
2023-11-26 14:14:51 -0300

    View methods

--- EventFlow/main_app/views.py
+++ EventFlow/main_app/views.py
@@ -1,13 +1,22 @@
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from .serializers import UserSerializer
 from .models import User
 from django.http import HttpResponse, JsonResponse
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def user_manager(request):
     if request.method == "GET":
         user = User.objects.all()
         serializer = UserSerializer(user, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == "POST":
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
