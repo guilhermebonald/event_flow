5cdb8dceef409223f000b1ef7ed94c3ee412d101
Guilherme Bonald <guilhermebonald@hotmail.com>
2023-11-28 21:11:24 -0300

    Update

--- EventFlow/main_app/models.py
+++ EventFlow/main_app/models.py
@@ -1,5 +1,26 @@
 from django.db import models
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 
 class User(models.Model):
     name = models.CharField(max_length=20)
+    owner = models.ForeignKey(
+        "auth.User", related_name="user", on_delete=models.CASCADE
+    )
+    highlighted = models.TextField()
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options
+        )
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
