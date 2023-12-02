015e6ffb9e39b0ac157ce182f9ca52c6d1f225f5
Guilherme Bonald <guilhermebonald@hotmail.com>
2023-12-02 20:17:16 -0300

    Updates

--- EventFlow/EventFlow/urls.py
+++ EventFlow/EventFlow/urls.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main_app import views
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path(
-        "login/",
-        views.login,
-    ),
-    path("signup/", views.signup),
-    path("auth_token/", views.test_token),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
