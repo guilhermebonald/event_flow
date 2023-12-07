67bee7a8dafd98ff4d12b2cbdfd44a416e2b5152
Guilherme Bonald <guilhermebonald@hotmail.com>
2023-12-06 21:07:35 -0300

    Updates

--- EventFlow/main_app/serializers.py
+++ EventFlow/main_app/serializers.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
 from .models import CustomUser
 
-# from .models import User
-
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = CustomUser
         fields = "__all__"
-        # or fields = ["name"]
--- EventFlow/main_app/views.py
+++ EventFlow/main_app/views.py
@@ -1,4 +1,3 @@
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from main_app.serializers import UserSerializer
@@ -7,6 +6,7 @@ from main_app.serializers import UserSerializer
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        return Response({"user": serializer.data})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
