47ca24f5a8ace109a7caff7bdf6dfc638d4107be
Guilherme Bonald <guilhermebonald@hotmail.com>
2023-12-08 12:54:15 -0300

    Updates

--- EventFlow/main_app/views.py
+++ EventFlow/main_app/views.py
@@ -1,9 +1,15 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from main_app.serializers import UserSerializer
+from .models import CustomUser
 
 
 class RegisterView(APIView):
+    def get(self, request):
+        user = CustomUser.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
+
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
